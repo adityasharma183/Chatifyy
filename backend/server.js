c3af0a4d4532ec8153a815e99de51bd0e18cac4b
Aditya Sharma <adityaa.sharma9757@gmail.com>
2026-01-27 16:26:53 +0530

    ready for deployment addded frontend in same file

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,29 @@
 import express from 'express'
 import dotenv from 'dotenv'
 dotenv.config()
+import path from 'path'
 import authRoute from './routes/authRoute.js'
 import messageRoute from './routes/messageRoute.js'
 const app=express()
+const __dirname=path.resolve();
 
 const PORT=process.env.PORT || 3000
+
+
 
 //middlewares for mounting routes
 app.use('/api/auth',authRoute)
 app.use('/api/messages',messageRoute)
 
+
+/// make ready for deployment
+if(process.env.NODE_ENV==="production"){
+    app.use(express.static(path.join(__dirname,"../frontend/dist")))
+
+    app.get('*',(req,res)=>{
+        res.sendFile(path.join(__dirname,"../frontend","dist","index.html"))
+    })
+}
 
 
 app.listen(PORT,()=>{
