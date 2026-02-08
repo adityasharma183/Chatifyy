added01bb88d157719a68f4049da052ade2716e8
Aditya Sharma <adityaa.sharma9757@gmail.com>
2026-02-08 23:41:15 +0530

    login page ,logout function added

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -6,6 +6,7 @@ export const useAuthStore=create((set)=>({
     authUser:null,
     isChecking:true,
     isSigningUp:false,
+    isLoggingIn:false,
 
     checkAuth:async()=>{
         try {
@@ -36,5 +37,33 @@ export const useAuthStore=create((set)=>({
             set({isSigningUp:false})
         }
 
+    },
+
+    login:async(data)=>{
+        set({isLoggingIn:true})
+        try {
+            const res=await axiosInstance.post('/auth/login',data)
+            set({authUser:res.data})
+
+            toast.success('Logged In successfully!!')
+            
+        } catch (error) {
+           toast.error(error.response.data.message)
+            
+            
+        }finally{
+            set({isLoggingIn:false})
+        }
+
+    },
+    logout:async()=>{
+        try {
+           await  axiosInstance.post('/auth/logout'),
+           set({authUser:null})
+           toast.success('Logged out successfully!!')
+
+        } catch (error) {
+            toast.error('Error logging out!!')
+        }
     }
 }))
